2bd46ca3bb215c965031d0eb760d255a1d474776
Chris Yarie <cyarie@aledade.com>
2021-12-09 16:28:43 -0500

    AOC-DAY1: Solved part two of day one.

--- src/advent_of_code_2021/day_1.clj
+++ src/advent_of_code_2021/day_1.clj
@@ -1,11 +1,20 @@
 (ns advent-of-code-2021.day-1
   (:gen-class)
-  (:require ['clojure.string :as 'str]))
+  (:require [clojure.string :as str]))
 
 (defn get-lines [file]
   (str/split-lines (slurp file)))
 
+; Part one
 (count (->> (get-lines "resources/day_1_input.txt")
      (map #(Integer/parseInt %))
+     (partition 2 1)
+     (mapcat (fn [[a b]] (if (> b a) [b])))))
+
+; Part two
+(count (->> (get-lines "resources/day_1_input.txt")
+     (map #(Integer/parseInt %))
+     (partition 3 1)
+     (mapcat (fn [[a b c]] [(+ a b c)]))
      (partition 2 1)
      (mapcat (fn [[a b]] (if (> b a) [b])))))
